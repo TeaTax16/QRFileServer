568eda14302002a1bbb2f8b606e7c709d42b9498
TeaTax16 <131816829+TeaTax16@users.noreply.github.com>
2025-01-03 10:54:22 +0000

    Removed Mail config as it's done from the email server side repo

--- application/mail_config.py
+++
@@ -1,7 +0,0 @@
-# Flask-Mail Configuration
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'Email'
-MAIL_PASSWORD = 'Mail App Password'
-MAIL_DEFAULT_SENDER = ('XARhub', 'Email')
